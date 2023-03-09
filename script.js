158eea033d1bd9b71675b6cdcf3aa52877be47f1
anand kumar <anand00kumar20@gmail.com>
2023-03-09 10:07:49 +0530

    basic change

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('anand')
+
+let user_name = prompt('what is your name?')
+
+let user_mobilenumber = prompt('what is your mobilenumber?')
+
+let user_cite = prompt('What is your cite?')
+
+//let user_followingnumber = prompt('What is your followingnumber?')
+
+document.querySelector('#name').innerHTML = user_name
+
+document.querySelector('#number').innerHTML = user_mobilenumber
+
+document.querySelector('#cite').innerHTML = user_cite
+
+//document.querySelector('#following').innerHTML = user_followingnumber
